38e728abc673d88f863eac598b2738926bb4b1cc
Jeff Kerpan <jkerpan@gmail.com>
2017-06-28 11:33:56 -0600

    3 tests passing

--- stack.js
+++ stack.js
@@ -1,5 +1,20 @@
 function Stack(){
   this.items = new Array();
+
 }
+
+Stack.prototype.empty =  function ()   {
+  return this.items.length === 0;
+};
+
+Stack.prototype.push = function (item)  {
+  this.items.push(item);
+  return item;
+};
+
+let peek = () => this.items(this.items.length -1);
+// return peek;
+
+
 
 module.exports = Stack;
